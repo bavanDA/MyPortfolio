438f3ea849d4d3930b7fa93dc160ea8d5f29b593
bavanDA <56412698+bavanDA@users.noreply.github.com>
2025-03-06 02:37:08 -0500

    feat(work-experience): add work experience page , card and data

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import Navbar from "@/components/navbar";
 import AboutMe from "./pages/about-me";
+import WorkExperience from "./pages/work-experience";
 
 const App: React.FC = () => {
   return (
     <>
       <Navbar />
-      <section id="about-me" className="">
+      <section id="about-me">
         <AboutMe />
       </section>
       <section id="my-projects" className="h-screen bg-gray-200 p-10">
         My Projects
       </section>
-      <section id="work-experience" className="h-screen bg-gray-300 p-10">
-        Work Experience
+      <section id="work-experience" className=" bg-gray-300 ">
+        <WorkExperience />
       </section>
       <section id="contact-me" className="h-screen bg-gray-400 p-10">
         Contact Me
