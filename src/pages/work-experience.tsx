17e9245113128dd2f01334488f434cb1fce78f13
bavanDA <56412698+bavanDA@users.noreply.github.com>
2025-03-12 23:58:14 -0400

    feat(project): refactor project and work experience pages for improved structure and readability

--- src/pages/work-experience.tsx
+++ src/pages/work-experience.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { experiences } from "@/data/experience";
 import TextSeparator from "@/components/text-separator";
 
@@ -9,79 +9,71 @@ const WorkExperience: React.FC = () => {
     <div className="max-w-4xl mx-auto p-4 space-y-8 dark:bg-gray-900">
       <TextSeparator text="Work Experience" />
 
-      {experiences.map((exp) => (
-        <div
-          key={exp.id}
-          className="bg-white dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden transition-transform duration-300 hover:shadow-xl"
-        >
-          {/* Header section */}
-          <div className="p-6 border-b border-gray-200 dark:border-gray-700">
-            <div className="flex flex-col sm:flex-row sm:justify-between sm:items-start">
-              <div>
-                <h3 className="text-xl font-bold text-gray-900 dark:text-gray-200">
-                  {exp.company}
-                </h3>
-                <h4 className="text-lg text-gray-700 dark:text-gray-400">
-                  {exp.position}
-                </h4>
-              </div>
-              <div className="mt-2 sm:mt-0">
-                <span className="inline-block bg-blue-100 dark:bg-blue-900 text-blue-800 dark:text-blue-300 text-sm font-semibold px-3 py-1 rounded-full">
-                  {exp.date}
+      {experiences.map(
+        ({ id, company, position, date, description, images }) => (
+          <div
+            key={id}
+            className="bg-white dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden transition-transform duration-300 hover:shadow-xl"
+          >
+            <div className="p-6 border-b border-gray-200 dark:border-gray-700">
+              <div className="flex flex-col sm:flex-row sm:justify-between sm:items-start">
+                <div>
+                  <h3 className="text-xl font-bold text-gray-900 dark:text-gray-200">
+                    {company}
+                  </h3>
+                  <h4 className="text-lg text-gray-700 dark:text-gray-400">
+                    {position}
+                  </h4>
+                </div>
+                <span className="mt-2 sm:mt-0 inline-block bg-blue-100 dark:bg-blue-900 text-blue-800 dark:text-blue-300 text-sm font-semibold px-3 py-1 rounded-full">
+                  {date}
                 </span>
               </div>
             </div>
-          </div>
 
-          {/* Description section */}
-          <div className="p-6">
-            <h5 className="text-sm font-medium text-gray-500 dark:text-gray-400 mb-3">
-              Responsibilities
-            </h5>
-            <ul className="space-y-2">
-              {exp.description.map((item, index) => (
-                <li key={index} className="flex items-start">
-                  <span className="text-blue-600 dark:text-blue-400 mr-2">
-                    •
-                  </span>
-                  <span className="text-gray-800 dark:text-gray-300">
-                    {item}
-                  </span>
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          {/* Images section - Only show if no image is selected */}
-          {exp.images && !selectedImage && (
-            <div className="p-6 border-b border-gray-200 dark:border-gray-700">
+            <div className="p-6">
               <h5 className="text-sm font-medium text-gray-500 dark:text-gray-400 mb-3">
-                Project Images
+                Responsibilities
               </h5>
-              <div className="flex overflow-x-auto pb-4 space-x-4">
-                {exp.images.map((img, index) => (
-                  <div
-                    key={index}
-                    className="flex-none cursor-pointer"
-                    onClick={() => setSelectedImage(img)}
-                  >
-                    <img
-                      src={img}
-                      alt={`${exp.company} project ${index + 1}`}
-                      className="w-32 object-cover rounded-lg shadow-lg transition-transform hover:scale-105 border-2 border-transparent hover:border-gray-500 dark:hover:border-gray-400"
-                    />
-                  </div>
+              <ul className="space-y-2">
+                {description.map((item, index) => (
+                  <li key={index} className="flex items-start">
+                    <span className="text-blue-600 dark:text-blue-400 mr-2">
+                      •
+                    </span>
+                    <span className="text-gray-800 dark:text-gray-300">
+                      {item}
+                    </span>
+                  </li>
                 ))}
-              </div>
+              </ul>
             </div>
-          )}
-        </div>
-      ))}
 
-      {/* Full-Screen Image View */}
+            {images && !selectedImage && (
+              <div className="p-6 border-b border-gray-200 dark:border-gray-700">
+                <h5 className="text-sm font-medium text-gray-500 dark:text-gray-400 mb-3">
+                  Project Images
+                </h5>
+                <div className="flex overflow-x-auto pb-4 space-x-4">
+                  {images.map((img, index) => (
+                    <img
+                      key={index}
+                      src={img}
+                      alt={`${company} project ${index + 1}`}
+                      className="w-32 object-cover rounded-lg shadow-lg transition-transform hover:scale-105 border-2 border-transparent hover:border-gray-500 dark:hover:border-gray-400 cursor-pointer"
+                      onClick={() => setSelectedImage(img)}
+                    />
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        )
+      )}
+
       {selectedImage && (
         <div
-          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 "
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75"
           onClick={() => setSelectedImage(null)}
         >
           <img
